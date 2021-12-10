12c3b705ad853d7d15a3e369cc706187432a2fe4
Pandi <pandi@wavetogether.com>
2021-12-10 11:27:49 +0900

    Movie_2

---
+++ src/routes/Home.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import MovieApp_2 from "./components/MovieApp_2";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+      )
+    ).json();
+
+    console.log(json);
+
+    setLoading(false);
+    setMovies(json.data.movies);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <MovieApp_2
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default Home;
